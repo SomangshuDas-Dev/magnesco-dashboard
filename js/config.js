57e5eacd16189bfff3fd953f75a1c10a7badb0fa
Somangshu Das <somangshudas.dev@gmail.com>
2026-03-04 23:47:52 +0530

    Initial commit

---
+++ js/config.js
@@ -0,0 +1,4 @@
+export const CONFIG = {
+    machineDataEndpoint: "https://script.google.com/macros/s/AKfycbwv_HjKYppweXZcMvZGMKT9mAPjdtNoHFHo91O2YVk2-FdnfdsJKKzNWsKqUmI9UJPp/exec", 	// Change according to your Google Apps Script "MAGNESCO Production Management System" Web App URL
+	dashboardEndpoint: "https://script.google.com/macros/s/AKfycbzfc_nJB7Nm_Otfstiz-5rFIQVJrx4V8W1ZennJgD5sCDgLte4eB10udacyB_7JFpuWew/exec" // Change according to your Google Apps Script "MAGNESCO Production Management System Dashboard" Web App URL
+};
